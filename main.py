1653382931a04021b268c2bad129e8c95dfc81e5
Arthur Soares <arthur.ext@gmail.com>
2020-01-15 18:54:13 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+from bs4 import BeautifulSoup
+
+if __name__ == "__main__":
+    import requests as rqs
+    from src.scraping.scraping import Scraping
+    
+    base_uri = r'https://www.basketball-reference.com/'
+    
+    srch = Scraping(base_uri)
+    srch.request()
+
+    content = srch.findUris()
+    srch.getUris()
+
+    print(srch.links)
+    
+    
+
+
+
+
+
+
+
+# import pandas as pd
+# import requests as rqs
+# from bs4 import BeautifulSoup
+
+# try: 
+#     req = rqs.get('https://www.basketball-reference.com/leagues/NBA_2018_totals.html')
+
+#     if req.status_code == 200:
+#         print("Requisição bem sucedida!")
+#         content = req.content
+#     else: 
+#         raise Exception("ops, houve um erro na requisição")
+# except Exception as ex:
+#     print(ex)
+#     raise
+# soup = BeautifulSoup(content, 'html.parser')
+# table = soup.find(name="table")
+
+# table_str = str(table)
+# df = pd.read_html(table_str)[0]
+# print(df)
---
+++ src/scraping/scraping.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import re
+
+class Scraping(object):
+    def __init__(self, base_uri):
+        self.__base_uri = base_uri
+
+    def request(self):
+        with webdriver.Chrome(executable_path=r"/usr/bin/chromedriver") as driver:
+            driver.get(self.__base_uri)
+            self.__content = BeautifulSoup(driver.page_source, "html.parser")
+
+    def findUris(self):
+        main_nav = self.__content.find(name="li", id="header_leagues")
+        return main_nav
+
+    def __removeSymbols(self, string):
+        return re.sub(r'[^\w]', '', string)
+
+
+    def getUris(self):
+        leagues = self.findUris()
+        leagues_content = leagues.findAll(name="div", class_="list")
+        
+        links = {}
+        for i in leagues_content:
+            season = i.find(name="span").getText()
+            season = self.__removeSymbols(season)
+            for j in i.findAll(name="a"):
+                if j.getText() == "Summary":
+                    links.update({ season: j['href'] })
+        self.__links = links
+
+    @property
+    def links(self):
+        return self.__links
