27a4a1b7752911183e4342450705f638b87c62e5
Arthur <arthur.ext@gmail.com>
2020-02-15 07:09:45 -0300

    refactoring Scraping class

--- src/scraping/scraping.py
+++ src/scraping/scraping.py
@@ -5,17 +5,24 @@ import re
 class Scraping(object):
     def __init__(self, base_uri):
         self.__base_uri = base_uri
-        self.__links = {}
+        self.__links = {},
+        self.__content = self.__getBaseSource()
 
     @property
     def links(self):
         return self.__links
 
+    @property
+    def content(self):
+        return self.__content
+
     def __removeSymbols(self, string):
         return re.sub(r'[^\w]', '', string)
 
-    def _getPageSource(self):
+    def getPageSource(self):
         with webdriver.Chrome(executable_path=r'/usr/bin/chromedriver') as driver:
-            driver.get()(self.__base_uri)
-            self.__content = BeautifulSoup(driver.page_source, 'html.parser')
-    
+            driver.get(self.__base_uri)
+            return driver.page_source
+
+    def __getBaseSource(self):
+        return self.getPageSource()
